68ec82261093ebeab1407a246b73d0bef73747a1
Wxrange <wloock283@gmail.com>
2023-04-19 22:08:47 -0500

    added comments for the all the methods

--- account.py
+++ account.py
@@ -17,7 +17,7 @@ class Account:
         """
         The method deposit allows the person to add money to the persons account
         :param amount: The amount of money the person wants to deposit
-        :return: boolean if deposit was successful
+        :return: boolean if deposit was successful or not
         """
         if amount <= 0:
             return False
@@ -26,6 +26,11 @@ class Account:
             return True
 
     def withdraw(self, amount: float):
+        """
+        withdraw method takes the amount the person wants out of their account
+        :param amount: is the amount of currency the person wants to take out of there account.
+        :return: boolean whether the withdraw was successful or not
+        """
         if self.__account_balance < amount or amount <= 0:
             return False
         else:
@@ -33,8 +38,16 @@ class Account:
             return True
 
     def get_balance(self) -> float:
+        """
+        get_balance gives the current amount of balance the aaccount has
+        :return: the value of balance
+        """
         return self.__account_balance
 
     def get_name(self) -> str:
+        """
+        get_name gets the name of the account holder
+        :return: string value the name given for the account
+        """
         return self.__account_name
 

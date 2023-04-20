11e7b223f20862e37152cbd00698cf1b06e5b497
Wxrange <wloock283@gmail.com>
2023-04-19 21:57:06 -0500

    add testing coed

--- account.py
+++ account.py
@@ -1,27 +1,40 @@
 
 class Account:
+    """
+    The Account class is meant to preform deposits and withdraws from a users account
+    """
 
-    def __init__(self, name, balance=0):
+    def __init__(self, name: str, balance=0):
+        """
+        Constructor is meant to assign the name and balance values to the account
+        :param name: persons first name
+        :param balance: persons balance
+        """
         self.__account_name = name
         self.__account_balance = balance
 
-    def deposit(self, amount):
+    def deposit(self, amount: float) -> bool:
+        """
+        The method deposit allows the person to add money to the persons account
+        :param amount: The amount of money the person wants to deposit
+        :return: boolean if deposit was successful
+        """
         if amount <= 0:
             return False
         else:
             self.__account_balance = self.__account_balance + amount
             return True
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float):
         if self.__account_balance < amount or amount <= 0:
             return False
         else:
             self.__account_balance = self.__account_balance - amount
             return True
 
-    def get_balance(self):
+    def get_balance(self) -> float:
         return self.__account_balance
 
-    def get_name(self):
+    def get_name(self) -> str:
         return self.__account_name
 
--- test_account.py
+++ test_account.py
@@ -1,2 +1,38 @@
 # testing
 # testing 2
+import pytest
+from account import *
+class test:
+    def setup_account(self):
+        self.account_one = Account("John")
+        self.account_two = Account("John", 1)
+    def teardown_account(self):
+        del self.account_one
+        del self.account_two
+    def test_deposit(self):
+        assert self.account_one.deposit(1) == True
+        assert self.account_one.deposit(-1) == False
+        assert self.account_one.deposit(1.0) == True
+        assert self.account_one.deposit(0) == False
+        assert self.account_one.deposit('t') == TypeError
+        assert self.account_two.deposit(1) == True
+        assert self.account_two.deposit(-1) == False
+        assert self.account_two.deposit(1.0) == True
+        assert self.account_two.deposit(0) == False
+        assert self.account_two.deposit('t') == TypeError
+    def test_withdraw(self):
+        assert self.account_one.withdraw(1) == True
+        assert self.account_one.deposit(-1) == False
+        assert self.account_one.deposit(1.0) == True
+        assert self.account_one.deposit(0) == False
+        assert self.account_one.deposit('t') == TypeError
+        assert self.account_two.withdraw(1) == True
+        assert self.account_two.deposit(-1) == False
+        assert self.account_two.deposit(1.0) == True
+        assert self.account_two.deposit(0) == False
+        assert self.account_two.deposit('t') == TypeError
+
+
+
+
+
